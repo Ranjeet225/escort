4a9f8b4721a434bf0da3958418b1198d1ea86d58
Ranjeet225 <ranjeetmaurya2033@gmail.com>
2025-03-04 22:24:36 +0530

    data

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,17 +1,17 @@
 @extends('layouts.main')
 
-@section('title', 'Home Page')
+@section('title', 'Dashboard Page')
 
 @section('css')
-    <link rel="stylesheet" href="{{ asset('assets/css/home.css') }}">
+    <link rel="stylesheet" href="{{ asset('assets/css/dashboard.css') }}">
 @endsection
 
 
 @section('content')
-    <h2>Welcome to the Home Page</h2>
+    <h2>Welcome to the Dashboard Page</h2>
     <p>This is the main content of the home page.</p>
 @endsection
 
 @section('js')
-    <script src="{{ asset('assets/js/home.js') }}"></script>
+    <script src="{{ asset('assets/js/dashboard.js') }}"></script>
 @endsection
